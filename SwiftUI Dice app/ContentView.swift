f9989a9e4cc8275bcb7190bd73de3593788b88b4
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2020-09-08 22:14:45 -0300

    first commit

---
+++ SwiftUI Dice app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Dice app
+//
+//  Created by Gustavo Lozano on 08/09/20.
+//  Copyright © 2020 gustavo.lozano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
