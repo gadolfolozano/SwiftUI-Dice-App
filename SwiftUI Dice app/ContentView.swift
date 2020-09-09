f4ed86b83d03b5e1453cfbd81bf3eb08303af005
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2020-09-08 23:11:54 -0300

    Created dice app using swift ui states

--- SwiftUI Dice app/ContentView.swift
+++ SwiftUI Dice app/ContentView.swift
@@ -9,13 +9,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    let diceImagesNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
+    @State var firstDiceImage = "DiceThree"
+    @State var secondDiceImage = "DiceFour"
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("GreenBackground")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("DiceeLogo")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(imageName: firstDiceImage)
+                    DiceView(imageName: secondDiceImage)
+                }
+            
+                Spacer()
+                
+                Button(action: {
+                    self.firstDiceImage = self.diceImagesNames.randomElement() ?? "DiceOne"
+                    self.secondDiceImage = self.diceImagesNames.randomElement() ?? "DiceOne"
+                }) {
+                    Text("Roll dices")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .font(Font.system(size: 30))
+                }
+                .padding(.horizontal, 20)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(height: 40)
+                        .foregroundColor(.red)
+                )
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let imageName: String
+    
+    var body: some View {
+        Image(imageName).resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
